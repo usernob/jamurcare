bd02e808cef393c3202eeae432163701136bdc4f
usernob <contact.ramzipandu@gmail.com>
2025-12-16 23:41:33 +0700

    fix proxy error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->append(\App\Http\Middleware\TrustProxies::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
