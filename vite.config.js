e60d6f91a6f983b2d01789b14f24e491c14e4140
usernob <contact.ramzipandu@gmail.com>
2025-12-17 12:47:26 +0700

    fix build error

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/dashboard/index.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+	plugins: [
+		laravel({
+			input: [
+				"resources/css/app.css",
+				"resources/js/app.js",
+				"resources/js/auth-common.js",
+				"resources/js/dashboard/index.js",
+			],
+			refresh: true,
+		}),
+		tailwindcss(),
+	],
 });
