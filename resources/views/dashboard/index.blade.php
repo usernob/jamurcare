280fc175a744a8f7b4173347ffcef2e4705e2fb5
usernob <contact.ramzipandu@gmail.com>
2025-12-23 13:39:56 +0700

    fix responsive

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" data-theme="dark">
+<html lang="en" data-theme="dark" class="scroll-smooth md:scrollable">
 
 <head>
     <meta charset="UTF-8">
@@ -19,126 +19,143 @@
     @vite('resources/js/dashboard/index.js')
 </head>
 
-<body class="bg-background text-on-surface font-poppins p-4 md:p-6 w-full h-screen min-w-[320px]">
-    <section class="flex flex-col xl:flex-row w-full h-full gap-4">
-        <aside
-            class="sticky flex flex-col gap-6 p-4 rounded-xl bg-surface text-on-surface h-full xl:w-[500px] border border-outline shadow-lg">
-            <div class="flex items-center gap-4">
-                <img src="{{ asset('img/logo-cropped.png') }}" class="size-12 bg-primary dark:bg-surface rounded-full"
-                    alt="Logo">
-                <h2 class="font-baloo font-semibold text-2xl">{{ config('app.name', 'Laravel') }}</h2>
-            </div>
-
-            <div class="flex items-center gap-4">
-                <img src="{{ $user->getAvatarUrlAttribute() }}" referrerpolicy="no-referrer"
-                    class="size-14 border-2 border-on-primary rounded-full object-cover object-center bg-primary"
-                    alt="Logo">
-                <div>
-                    <h2 class="font-semibold">Hello {{ Str::limit($user->name, 20, preserveWords: true) }}</h2>
-                    <p class="text-on-surface/60">Get Ready to plant</p>
+<body class="bg-background text-on-surface font-poppins w-full min-w-[320px]">
+    <section class="flex flex-col md:flex-row w-full h-full gap-4 p-4 md:p-0">
+        <div class="md:sticky top-0 md:p-6 md:pr-0 md:h-screen">
+            <aside
+                class="flex flex-col gap-6 p-4 rounded-xl bg-surface text-on-surface h-full md:w-[300px] xl:w-[400px] border border-outline shadow-lg">
+                    <div class="flex items-center justify-between">
+                <div class="flex items-center gap-4">
+                    <img src="{{ asset('img/logo-cropped.png') }}"
+                        class="size-12 bg-primary dark:bg-surface rounded-full" alt="Logo">
+                    <h2 class="font-baloo font-semibold text-2xl">{{ config('app.name', 'Laravel') }}</h2>
                 </div>
-            </div>
+                <div class="flex md:hidden gap-2 items-center">
+                    <button class="material-symbols-outlined !text-2xl cursor-pointer"
+                        onclick="logout()">logout</button>
+                    <div class="relative size-8 cursor-pointer" id="darkmode-toggler">
+                        <span class="absolute material-symbols-outlined !text-2xl !hidden"
+                            id="icon-dark">dark_mode</span>
+                        <span class="absolute material-symbols-outlined !text-2xl !hidden"
+                            id="icon-light">light_mode</span>
+                    </div>
+                </div>
+                    </div>
 
-            <div class="overflow-y-scroll h-full flex flex-col gap-4 scrollable">
-                <div>
-                    <h2 class="font-semibold mb-4 text-lg">Device</h2>
-                    <div class="bg-surface-container rounded-xl w-full flex flex-col overflow-hidden"
-                        id="dropdown-device">
-                        <div class="flex items-center justify-between gap-4 px-4 py-2 cursor-pointer"
-                            data-dropdown-trigger>
-                            <div>
-                                <h2 class="font-semibold">{{ $current_device->name }}</h2>
-                                <h3 class="text-sm">Status: <span id="device-status">Loading...</span></h3>
-                            </div>
-
-                            <i
-                                class="material-symbols-outlined !text-2xl arrow-drop transition-all duration-400 ease-out">
-                                expand_more
-                            </i>
-                        </div>
-
-                        <div class="flex flex-col border-t border-on-surface/20 dark:border-outline/60 divide-y divide-on-surface/20 dark:divide-outline/60 w-full transition-all ease-out"
-                            data-dropdown-menu>
-                            @foreach ($user->devices as $device)
-                                @if ($device->id != $current_device->id)
-                                    <a class="px-4 py-2 hover:bg-black/20 cursor-pointer"
-                                        href="{{ route('dashboard.index', $device->ulid) }}">
-                                        <h2>{{ $device->name }}</h2>
-                                    </a>
-                                @endif
-                            @endforeach
-                            <a class="px-4 py-2 hover:bg-black/20 cursor-pointer"
-                                href="{{ route('device.add.form') }}">
-                                <div class="flex items-center gap-1">
-                                    <h2 class="material-symbols-outlined !text-xl">add</h2>
-                                    <h2>Add Device</h2>
-                                </div>
-                            </a>
-                        </div>
+                <div class="flex items-center gap-4">
+                    <img src="{{ $user->getAvatarUrlAttribute() }}" referrerpolicy="no-referrer"
+                        class="size-14 border-2 border-on-primary rounded-full object-cover object-center bg-primary"
+                        alt="Logo">
+                    <div>
+                        <h2 class="font-semibold">Hello {{ Str::limit($user->name, 20, preserveWords: true) }}</h2>
+                        <p class="text-on-surface/60">Get Ready to plant</p>
                     </div>
                 </div>
 
-                <div>
-                    <h2 class="font-semibold mb-4 text-lg">Auto Scenes</h2>
-                    <div class="flex flex-col items-center gap-2 w-full">
-                        @foreach (['pump', 'lamp', 'fan'] as $scene)
-                            <div class="bg-surface-container rounded-[1.625rem] w-full overflow-hidden"
-                                id="{{ $scene }}-state" data-state="{{ $scene }}" data-dropdown>
-                                <div class="relative flex items-center gap-4 p-1 pr-4 cursor-pointer"
-                                    data-dropdown-trigger>
-                                    <div
-                                        class="bg-primary-container rounded-full size-12 flex items-center justify-center">
-                                        <i class="material-symbols-outlined text-primary !text-3xl">
+                <div class="overflow-y-scroll h-full flex flex-col gap-4 scrollable">
+                    <div>
+                        <h2 class="font-semibold mb-4 text-lg">Device</h2>
+                        <div class="bg-surface-container rounded-xl w-full flex flex-col overflow-hidden"
+                            id="dropdown-device">
+                            <div class="flex items-center justify-between gap-4 px-4 py-2 cursor-pointer"
+                                data-dropdown-trigger>
+                                <div>
+                                    <h2 class="font-semibold">{{ $current_device->name }}</h2>
+                                    <h3 class="text-sm">Status: <span id="device-status">Loading...</span></h3>
+                                </div>
+
+                                <i
+                                    class="material-symbols-outlined !text-2xl arrow-drop transition-all duration-400 ease-out">
+                                    expand_more
+                                </i>
+                            </div>
+
+                            <div class="flex flex-col border-t border-on-surface/20 dark:border-outline/60 divide-y divide-on-surface/20 dark:divide-outline/60 w-full transition-all ease-out"
+                                data-dropdown-menu>
+                                @foreach ($user->devices as $device)
+                                    @if ($device->id != $current_device->id)
+                                        <a class="px-4 py-2 hover:bg-black/20 cursor-pointer"
+                                            href="{{ route('dashboard.index', $device->ulid) }}">
+                                            <h2>{{ $device->name }}</h2>
+                                        </a>
+                                    @endif
+                                @endforeach
+                                <a class="px-4 py-2 hover:bg-black/20 cursor-pointer"
+                                    href="{{ route('device.add.form') }}">
+                                    <div class="flex items-center gap-1">
+                                        <h2 class="material-symbols-outlined !text-xl">add</h2>
+                                        <h2>Add Device</h2>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h2 class="font-semibold mb-4 text-lg">Auto Scenes</h2>
+                        <div class="flex flex-col items-center gap-2 w-full">
+                            @foreach (['pump', 'lamp', 'fan'] as $scene)
+                                <div class="bg-surface-container rounded-[1.625rem] w-full overflow-hidden"
+                                    id="{{ $scene }}-state" data-state="{{ $scene }}" data-dropdown>
+                                    <div class="relative flex items-center gap-4 p-1 pr-4 cursor-pointer"
+                                        data-dropdown-trigger>
+                                        <div
+                                            class="bg-primary-container rounded-full size-12 flex items-center justify-center">
+                                            <i class="material-symbols-outlined text-primary !text-3xl">
+                                                @if ($scene === 'pump')
+                                                    water_drop
+                                                @elseif ($scene === 'lamp')
+                                                    lightbulb_2
+                                                @else
+                                                    toys_fan
+                                                @endif
+                                            </i>
+                                        </div>
+                                        <h2 class="font-semibold">
                                             @if ($scene === 'pump')
-                                                water_drop
+                                                Irigation
                                             @elseif ($scene === 'lamp')
-                                                lightbulb_2
+                                                Lamp
                                             @else
-                                                toys_fan
+                                                Fan
                                             @endif
-                                        </i>
+                                        </h2>
+                                        <div class="grow"></div>
+                                        <div class="font-semibold state-label">AUTO</div>
+                                        <div
+                                            class="state-load hidden absolute top-1/2 -translate-y-1/2 right-3 flex animate-spin">
+                                            <i class="material-symbols-outlined">cached</i>
+                                        </div>
                                     </div>
-                                    <h2 class="font-semibold">
-                                        @if ($scene === 'pump')
-                                            Irigation
-                                        @elseif ($scene === 'lamp')
-                                            Lamp
-                                        @else
-                                            Fan
-                                        @endif
-                                    </h2>
-                                    <div class="grow"></div>
-                                    <div class="font-semibold state-label">AUTO</div>
-                                    <div
-                                        class="state-load hidden absolute top-1/2 -translate-y-1/2 right-3 flex animate-spin">
-                                        <i class="material-symbols-outlined">cached</i>
+                                    <div data-dropdown-menu
+                                        class="flex flex-col border-t border-on-surface/20 dark:border-outline/60 divide-y divide-on-surface/20 dark:divide-outline/60 transition-all ease-out">
+                                        <h2 class="px-4 py-2 hover:bg-black/20 cursor-pointer font-semibold">OFF</h2>
+                                        <h2 class="px-4 py-2 hover:bg-black/20 cursor-pointer font-semibold">ON</h2>
+                                        <h2 class="px-4 py-2 hover:bg-black/20 cursor-pointer font-semibold">AUTO</h2>
                                     </div>
                                 </div>
-                                <div data-dropdown-menu
-                                    class="flex flex-col border-t border-on-surface/20 dark:border-outline/60 divide-y divide-on-surface/20 dark:divide-outline/60 transition-all ease-out">
-                                    <h2 class="px-4 py-2 hover:bg-black/20 cursor-pointer font-semibold">OFF</h2>
-                                    <h2 class="px-4 py-2 hover:bg-black/20 cursor-pointer font-semibold">ON</h2>
-                                    <h2 class="px-4 py-2 hover:bg-black/20 cursor-pointer font-semibold">AUTO</h2>
-                                </div>
-                            </div>
-                        @endforeach
+                            @endforeach
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="flex items-center justify-between pt-2 mt-auto">
-                <button class="material-symbols-outlined !text-3xl cursor-pointer" onclick="logout()">logout</button>
-                <div class="relative size-8 cursor-pointer" id="darkmode-toggler">
-                    <span class="absolute material-symbols-outlined !text-3xl !hidden" id="icon-dark">dark_mode</span>
-                    <span class="absolute material-symbols-outlined !text-3xl !hidden" id="icon-light">light_mode</span>
+                <div class="hidden md:flex items-center justify-between pt-2 mt-auto">
+                    <button class="material-symbols-outlined !text-3xl cursor-pointer"
+                        onclick="logout()">logout</button>
+                    <div class="relative size-8 cursor-pointer" id="darkmode-toggler">
+                        <span class="absolute material-symbols-outlined !text-3xl !hidden"
+                            id="icon-dark">dark_mode</span>
+                        <span class="absolute material-symbols-outlined !text-3xl !hidden"
+                            id="icon-light">light_mode</span>
+                    </div>
                 </div>
-            </div>
-        </aside>
+            </aside>
+        </div>
 
         <!-- ========== MAIN CONTENT ========== -->
-        <div class="w-full h-fit flex flex-col gap-4">
+        <div class="w-full h-fit flex flex-col gap-4 md:p-6 md:pl-0">
             <!-- Real-time Monitoring (2 kolom di desktop) -->
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
                 <!-- Suhu -->
                 <div class="rounded-xl border border-outline p-4 bg-surface shadow-lg">
                     <div class="flex justify-between items-start">
@@ -208,7 +225,8 @@
                                     {{ $date }}
                                 </span>
                                 @if ($isToday)
-                                    <span class="text-[10px] bg-primary text-white dark:text-gray-900 font-semibold px-1 py-0.5 rounded">Hari
+                                    <span
+                                        class="text-[10px] bg-primary text-white dark:text-gray-900 font-semibold px-1 py-0.5 rounded">Hari
                                         Ini</span>
                                 @endif
                             </div>
